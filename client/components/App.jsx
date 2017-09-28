6760817a17b9d761b9acde9821234b6d7a47e14b
Juan Alcala <juan.alcala@unosquare.com>
2017-09-28 12:48:33 -0500

    code reduction/styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -43,38 +43,34 @@ import ActionCheckCircle  from  'material-ui/svg-icons/action/check-circle'
 import SelectAllIcon from 'material-ui/svg-icons/content/select-all';
 
 
-
+const Filter = {
+    ALL: 0,
+    ACTIVE: 1,
+    COMPLETED: 2
+};
 
 const styles = {
-  
+
     radioButton: {
         marginBottom: 16,
-        width:'auto'
+        width: 'auto'
     },
-  unSelectedItemStyle :{
-        textDecoration:'none',
-        color:'rgba(0,0,0,1)',
-        fontSize:'2em'
+    unSelectedItemStyle: {
+        textDecoration: 'none',
+        color: 'rgba(0,0,0,1)',
+        fontSize: '2em'
     },
-    selectedItemStyle :{
-        textDecoration:'line-through',
-        color:'rgba(0,0,0,0.5)',
-        fontSize:'2em'
+    selectedItemStyle: {
+        textDecoration: 'line-through',
+        color: 'rgba(0,0,0,0.5)',
+        fontSize: '2em'
     }
 };
-class Item extends React.Component{
 
-    
+class Item extends React.Component{  
+    onDelete = (ev)=> this.props.onDelete(this.props.id);
 
-    onDelete = (ev)=>{
-       
-        this.props.onDelete(this.props.id);
-    };
-    onCheckboxCliked = (ev,isChecked)=>{
-      
-        
-       this.props.itemStatusChanged(this.props.id);
-    };
+    onCheckboxCliked = (ev,isChecked)=> this.props.itemStatusChanged(this.props.id);
 
     render(){
         return (    
@@ -91,181 +87,19 @@ class Item extends React.Component{
     }
   
 }
-const Filter = {
-    ALL:0,
-    ACTIVE:1,
-    COMPLETED:2
-};
-class  ItemList extends React.Component{
-
-    state ={
-      items:[],
-      filteredItems:[],
-      selected:false,
-      activeFilter: Filter.ALL
-    };
-    filter = (f)=>{
-      
-       
-        this.setState({
-            filteredItems: _.filter(this.state.items,(item)=>{
-
-                switch(f){
-                    case Filter.COMPLETED:
-                       
-                        return item.done?item:null;
-                    break;
-                   
-                    case Filter.ACTIVE:
-                    return item.done?null:item;
-                    break;
-                    default:
-                   
-                    return item;
-                    break;
-                }
-            }),
-            activeFilter:f
-        });
-    };
-     addNewitem= (name)=>{   
-       
-        this.setState(prevState=>({
-            items: [...prevState.items, {name:name,done:false,id:prevState.items.length}],
-          
-        }),()=>this.filter(this.state.activeFilter));
-
-    };
-
-    onDelete = (item)=>{
-       
-        this.setState(prevState =>({
-           
-            items: _.filter(prevState.items,(i,n)=>{
-               
-                return i.id !== item;
-            }),
-            filteredItems:_.filter(prevState.filteredItems,(i,n)=>{
-               
-                return i.id !== item;
-            })
-            }));
-    };
-    itemStatusChanged = (item)=>{
-      
-        this.setState(prevState =>({
-            
-             items:_.map(prevState.items,(val,i)=>{
-
-               
-                 if(val.id===item)
-                 {
-                    
-                    
-                   val.done = val.done?false:true;
-                 }
-                 return val;
-             })
-             }),()=>this.filter(this.state.activeFilter));
-    };
-    selectAll =()=>{
-       
-        this.setState(prevState =>({
-            
-             items:_.map(prevState.items,(val,i)=>{
-                 this.state.selected ?val.done=false: val.done=true;             
-                 
-                 return val;
-             }),
-             selected:!prevState.selected
-            }),()=>this.filter(this.state.activeFilter));
-    };
-    render() {
-    return (
-      <Grid  >       
-        <Form selected={this.state.selected} selectAll={this.selectAll} onSubmit={this.addNewitem}/>
-        
-        
-        <Row center={'xs'} >
-        <Col   xs={12} sm={8} md={6} lg={5}>
-        
-            <MuiThemeProvider >                  
-                <List  >            
-                    {
-                 
-                    this.state.filteredItems.map((item,i) => {
-                        
-                        return  <Item 
-                        itemStatusChanged={this.itemStatusChanged} 
-                        done={item.done} onDelete={this.onDelete} 
-                        id={item.id} 
-                        key={i}  
-                        name={item.name}/>})
-                  
-                    }
-                </List>
-            </MuiThemeProvider >
-            
-        </Col>
-    </Row>
-       
-        <Row center={'xs'} >
-            <Col   xs={12} sm={8} md={6} lg={5}>
-                <MuiThemeProvider > 
-               
-                    <Toolbar >     
-                  
-                        <ToolbarGroup firstChild>
-                           
-                        <Badge
-                        badgeContent={_.reduce(this.state.items, function(memo, num){ return num.done?memo: memo + 1 }, 0)}
-                        secondary={true}
-                        badgeStyle={_.reduce(this.state.items, function(memo, num){ return num.done?memo: memo + 1 }, 0)>0?{top: 20, right: 20}:{display:'none'}}
-                        >
-                            <IconButton tooltip="Items left">
-                            <NotificationsIcon />
-                            </IconButton>
-                    </Badge>
-                    </ToolbarGroup>
-                    <ToolbarGroup >
-                    <IconButton tooltip="Select All" onClick={()=>this.filter(Filter.ALL)} touch={true} tooltipPosition='bottom-center' ><SelectAllIcon/></IconButton> 
-                    
-                   
-                   
-                    <IconButton tooltip="Active" onClick={()=>this.filter(Filter.ACTIVE)} touch={true} tooltipPosition='bottom-center' ><ScheduleIcon/></IconButton> 
-                   
-                    <IconButton tooltip="Completed" onClick={()=>this.filter(Filter.COMPLETED)} touch={true} tooltipPosition='bottom-center' ><DoneIcon/></IconButton> 
-                    
-                    </ToolbarGroup>
-                       
-                       
-           
-          
-      </Toolbar>
-     
-      
-      </MuiThemeProvider >
-      </Col>
-        </Row>
-        
-            
-      </Grid>
-    );
-  }
-  
-    
-} ;
 class Form extends React.Component{
-    state={item:''}
+    state = {
+        item: ''
+    }
 
-    handleSubmit = (event)=>{
+    handleSubmit = (event) => {
         event.preventDefault();
         this.props.onSubmit(this.state.item);
-        this.setState({item:''});
+        this.setState({item: ''});
     };
-    selectAll =()=>{
-        this.props.selectAll();
-    };
+
+    selectAll =()=> this.props.selectAll();
+    
     render(){    
         return(
         <Row center='xs'  >
@@ -297,14 +131,144 @@ class Form extends React.Component{
 }
 
 
-class ToDoList extends React.Component {
-   
-  render(){
+class  ItemList extends React.Component{
+    state = {
+        items: [],
+        filteredItems: [],
+        selected: false,
+        activeFilter: Filter.ALL
+    };
+    filter = (f)=>{
+        this.setState({
+            filteredItems : _.filter(this.state.items, (item) => {
 
+                switch (f) {
+                    case Filter.COMPLETED:
+                        return item.done
+                            ? item
+                            : null;
+                        break;
+
+                    case Filter.ACTIVE:
+                        return item.done
+                            ? null
+                            : item;
+                        break;
+                    default:
+
+                        return item;
+                        break;
+                }
+            }),
+            activeFilter : f
+        });
+    };
+     addNewitem= (name)=>{   
+       
+        this.setState(prevState=>({
+            items: [...prevState.items, {name:name,done:false,id:prevState.items.length}],
+          
+        }),()=>this.filter(this.state.activeFilter));
+
+    };
+
+    onDelete = (item)=>{
+       
+        this.setState(prevState =>({
+           
+            items: _.filter(prevState.items,(i,n)=>{
+               
+                return i.id !== item;
+            }),
+            filteredItems:_.filter(prevState.filteredItems,(i,n)=>{
+               
+                return i.id !== item;
+            })
+            }));
+    };
+    itemStatusChanged = (item)=>{
+      
+        this.setState(prevState =>({
+            
+             items:_.map(prevState.items,(val,i)=>{
+                val.done = (val.id==item ? val.done? false:true:val.done); 
+                 return val;
+             })
+        }),()=>this.filter(this.state.activeFilter));
+    };
+    selectAll =()=>{       
+        this.setState(prevState =>({
+            
+             items:_.map(prevState.items,(val,i)=>{
+                 this.state.selected ?val.done=false: val.done=true;
+                 return val;
+             }),             
+             selected:!prevState.selected
+        }),()=>this.filter(this.state.activeFilter));
+    };
+    render() {
     return (
-        <ItemList/>
-    )
+    <Grid  >       
+        <Form selected={this.state.selected} selectAll={this.selectAll} onSubmit={this.addNewitem}/>
+        <Row center={'xs'} >
+            <Col   xs={12} sm={8} md={6} lg={5}>
+                <MuiThemeProvider >                  
+                    <List  >            
+                        {                 
+                        this.state.filteredItems.map((item,i) => {
+                            
+                            return  <Item 
+                            itemStatusChanged={this.itemStatusChanged} 
+                            done={item.done} onDelete={this.onDelete} 
+                            id={item.id} 
+                            key={i}  
+                            name={item.name}/>})                  
+                        }
+                    </List>
+                </MuiThemeProvider >                
+            </Col>
+        </Row>
+       
+        <Row center={'xs'} >
+            <Col   xs={12} sm={8} md={6} lg={5}>
+                <MuiThemeProvider > 
+               
+                    <Toolbar >                      
+                        <ToolbarGroup firstChild>                           
+                            <Badge
+                            badgeContent={_.reduce(this.state.items, function(memo, num){ return num.done?memo: memo + 1 }, 0)}
+                            secondary={true}
+                            badgeStyle={_.reduce(this.state.items, function(memo, num){ return num.done?memo: memo + 1 }, 0)>0?{top: 20, right: 20}:{display:'none'}}
+                            >
+                            <IconButton tooltip="Items left">
+                                <NotificationsIcon />
+                            </IconButton>
+                            </Badge>
+                        </ToolbarGroup>
+
+                        <ToolbarGroup >
+                        <IconButton tooltip="Select All"  onClick={()=>this.filter(Filter.ALL)} touch={true} tooltipPosition='bottom-center' ><SelectAllIcon/></IconButton>   
+                            <IconButton tooltip="Active" onClick={()=>this.filter(Filter.ACTIVE)} touch={true} tooltipPosition='bottom-center' ><ScheduleIcon/></IconButton>                         
+                            <IconButton tooltip="Completed" onClick={()=>this.filter(Filter.COMPLETED)} touch={true} tooltipPosition='bottom-center' ><DoneIcon/></IconButton>                        
+                        </ToolbarGroup>
+                    </Toolbar>
+                </MuiThemeProvider >
+            </Col>
+        </Row>   
+    </Grid>
+    );
   }
+  
+    
+} ;
+
+
+class ToDoList extends React.Component {
+
+    render() {
+
+        return (<ItemList/>)
+    }
 }
 
 export default ToDoList;
